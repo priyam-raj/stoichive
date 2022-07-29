7cf4d822a50e96339ffaa8108569b4848058f0f5
Priyam Raj <hello@priyamraj.com>
2022-07-30 00:10:24 +0530

    + image construction

--- createImage.js
+++ createImage.js
@@ -16,4 +16,43 @@ async function getQuote() {
     }
 
 
-    getQuote()
+    async function buildImage() {
+        let fetchedData = await getQuote();
+
+        
+        let todaysQuote = fetchedData[0];
+        let todaysAuthor = fetchedData[1];
+        todaysAuthor = "- " + todaysAuthor;
+
+        console.log(todaysQuote + todaysAuthor);
+      
+        let font = await Jimp.loadFont(`assets/philosopher.fnt`);
+      
+      
+        Jimp.read('assets/postBackground.jpg', (err, image) => {
+          if (err) throw err;
+          Jimp.loadFont(`assets/philosopher.fnt`, (err, font) => {
+            var w = image.bitmap.width;
+            var h = image.bitmap.height;
+            var textWidth = Jimp.measureText(font, todaysQuote);
+            var textHeight = Jimp.measureTextHeight(font, todaysQuote);
+            var textWidth2 = Jimp.measureText(font, todaysAuthor);
+            var textHeight2 = Jimp.measureTextHeight(font, todaysAuthor);
+            image
+              .print(font, w/2 - textWidth/2, h/2 - textHeight/2 - 50,
+                {   
+                text: todaysQuote,
+                }, textWidth, textHeight)
+                .print(font, w/2 - textWidth2/2, textHeight2/2 + h/2 + 500,
+                {   
+                text: todaysAuthor,
+                }, textWidth2, textHeight2)
+              .write('todaysPost.jpg'); // save
+          }); 
+        });
+      
+      
+      }
+
+
+      buildImage()
