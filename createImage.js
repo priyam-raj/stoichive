cbfd2bd34df38ecf1dde375b5e13c6b542830b07
Priyam Raj <hello@priyamraj.com>
2022-07-30 00:43:36 +0530

    Fixed words breaking in middle

--- createImage.js
+++ createImage.js
@@ -2,6 +2,8 @@ import fetch from "node-fetch";
 import Jimp from "jimp";
 
 
+
+
 async function getQuote() {
     let todaysData = await fetch("https://stoicquotesapi.com/v1/api/quotes/random")
       .then((res) => res.json())
@@ -16,16 +18,20 @@ async function getQuote() {
     }
 
 
-async function splitString(string) {
-    let stringArray = [];
-    let stringLength = string.length;
-    let stringArrayLength = Math.ceil(stringLength/25);
-    for (let i = 0; i < stringArrayLength; i++) {
-        let stringSubstring = string.substring(i*25, (i+1)*25);
-        stringArray.push(stringSubstring);
+function splitString(string) {
+    let stringArray = string.split(" ");
+    let newArray = [];
+    let tempString = "";
+    for (let i = 0; i < stringArray.length; i++) {
+        if (tempString.length + stringArray[i].length + 1 > 30) {
+            newArray.push(tempString);
+            tempString = "";
+        }
+        tempString += stringArray[i] + " ";
     }
-    console.log(stringArray);
-    return stringArray;
+    newArray.push(tempString);
+    return newArray;
+
 }
 
 
@@ -34,34 +40,27 @@ async function splitString(string) {
         let fetchedData = await getQuote();
         let todaysQuote = fetchedData[0];
         todaysQuote = await splitString(todaysQuote);
-        console.log(todaysQuote);
-        
         let todaysAuthor = fetchedData[1];
         todaysAuthor = "- " + todaysAuthor;
 
         console.log(todaysQuote + todaysAuthor);
-      
-        let font = await Jimp.loadFont(`assets/philosopher.fnt`);
-      
-      
+            
+        const font = `assets/philosopher.fnt`;
+
         Jimp.read('assets/postBackground.jpg', (err, image) => {
           if (err) throw err;
           Jimp.loadFont(`assets/philosopher.fnt`, (err, font) => {
             var w = image.bitmap.width;
             var h = image.bitmap.height;
-            var textWidth = Jimp.measureText(font, todaysQuote);
-            var textHeight = Jimp.measureTextHeight(font, todaysQuote);
             var textWidth2 = Jimp.measureText(font, todaysAuthor);
-            var textHeight2 = Jimp.measureTextHeight(font, todaysAuthor);
-            image
-
-              .print(font, w/2 - textWidth/2, h/2 - textHeight/2 - 50,
-                {   
-                text: todaysQuote,
-                }, textWidth, textHeight)
-                .print(font, w/2 - textWidth2/2, textHeight2/2 + h/2 + 500,
-
-
+            var textHeight2 = Jimp.measureTextHeight(font, todaysAuthor);   
+                    
+            for (let i = 0; i < todaysQuote.length; i++) {
+              var textHeight = Jimp.measureTextHeight(font, todaysQuote[i]);
+              var textWidth = Jimp.measureText(font, todaysQuote[i]);
+                image.print(font, w/2 - textWidth/2, h/5 - textHeight/2 + i*textHeight, todaysQuote[i]);
+            }
+                image.print(font, w/2 - textWidth2/2, textHeight2/2 + h/2 + 500,
                 {   
                 text: todaysAuthor,
                 }, textWidth2, textHeight2)
